4996018dd1f891b093291f93636b5bf33fe7d918
ahmedhosam-dev <ahmed.hossam.t.11@gmail.com>
2024-05-24 19:16:06 +0300

    Upload project files

---
+++ index.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// Connect to database
+mongoose.connect(process.env.DB_URL);
+
+// Database model
+const urlSchema = new mongoose.Schema({
+  orignUrl: String,
+  date: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  click: Number,
+});
+
+const UrlModel = mongoose.model("UrlModel", urlSchema);
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// Your first API endpoint
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+
+app.get("/api/shorturl/:id", async (req, res) => {
+  try {
+    let orignUrl = await UrlModel.findById(req.params.id).then( async (data, err) => {
+      if (err) return res.status(404).json({ error_message: err });
+      data.click += 1;
+      await data.save();
+      res.redirect(data.orignUrl);
+    });
+  } catch (e) {
+    res.status(500).json({ message: e });
+  }
+});
+
+app.post("/api/shorturl", async (req, res) => {
+  try {
+    const newUrl = new UrlModel({
+      orignUrl: req.body.url,
+      click: 0,
+    });
+
+    await newUrl.save().then(() => {
+      console.log("New url added!");
+    });
+
+    res.json({
+      original_url: req.body.url,
+      short_url: `${req.hostname}/api/shorturl/${newUrl._id}`,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// Listen port
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
