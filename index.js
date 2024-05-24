815133384020c7941cc8436f751762c364896365
ahmedhosam-dev <ahmed.hossam.t.11@gmail.com>
2024-05-24 20:45:48 +0300

    Check url before save data

--- index.js
+++ index.js
@@ -56,6 +56,19 @@ app.get("/api/shorturl/:id", async (req, res) => {
 
 app.post("/api/shorturl", async (req, res) => {
   try {
+
+    let urlPattern = new RegExp(
+      "^(https?:\\/\\/)?" + // validate protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    );
+
+    if (!urlPattern.test(urlString)) return res.status(400).json({ error: "invalid url" })
+
     const newUrl = new UrlModel({
       orignUrl: req.body.url,
       click: 0,
